e1ae395f49e6b60dfcad0836c5f8a6856acc65b5
Matthew Ashton <mashton237@fullsail.com>
2013-08-06 19:11:35 -0400

    Added VFW Project 4 files to new MUI file

---
+++ js/json.js
@@ -0,0 +1,45 @@
+// Matthew Ashton
+// VFW Project 4
+// JSON Object
+
+
+
+var jsonObject = {
+    "game1": {
+        "gameName": ["Name of Game:", "Revolution vs DC United"],
+        "homeTeam": ["Home Team:", "Revolution"],
+        "awayTeam": ["Away Team:", "DC United"],
+        "group": ["Group:", "Soccer"],
+        "other": ["Other:", ""],
+        "favTeam": ["Favorite Team:", "Yes"],
+        "priority": ["Game Priority:", "5"],
+        "dateOfGame": ["Date of Game:", "2013-06-26"],
+        "winningTeam": ["Winning Team:", "Revolution"]
+    },
+    "game2": {
+        "gameName": ["Name of Game:", "Angels vs Tigers"],
+        "homeTeam": ["Home Team:", "Angels"],
+        "awayTeam": ["Away Team:", "Tigers"],
+        "group": ["Group:", "Baseball"],
+        "other": ["Other:", ""],
+        "favTeam": ["Favorite Team:", "No"],
+        "priority": ["Game Priority:", "2"],
+        "dateOfGame": ["Date of Game:", "2013-06-27"],
+        "winningTeam": ["Winning Team:", "Tigers"]
+    },
+    "game3": {
+        "gameName": ["Name of Game:", "Spain vs Italy"],
+        "homeTeam": ["Home Team:", "Spain"],
+        "awayTeam": ["Away Team:", "Italy"],
+        "group": ["Group:", "Soccer"],
+        "other": ["Other:", ""],
+        "favTeam": ["Favorite Team:", "No"],
+        "priority": ["Game Priority:", "5"],
+        "dateOfGame": ["Date of Game:", "2013-06-27"],
+        "winningTeam": ["Winning Team:", "Italy"]
+    }
+}
+
+
+
+
